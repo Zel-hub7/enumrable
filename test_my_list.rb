bfa06b05544f80914eb3a810df9280318ac2a6b0
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-10-06 15:37:41 +0300

    add solutions

---
+++ test_my_list.rb
@@ -0,0 +1,15 @@
+require_relative 'my_list'
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts list.all? { |e| e < 5 } # Should return true
+puts list.all? { |e| e > 5 } # Should return false
+
+# Test #any?
+puts list.any? { |e| e == 2 } # Should return true
+puts list.any? { |e| e == 5 } # Should return false
+
+# Test #filter
+puts list.filter { |e| e.even? } # Should return [2, 4]
